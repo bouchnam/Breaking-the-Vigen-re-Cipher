9adaf7f46c8075c1e502c6993468fb4ba5d9e3cb
bouchnam <MOUAD.BOUCHNAF@GRENOBLE-inp.org>
2021-10-31 15:03:48 +0100

    Unknown length

--- VigenereBreaker.java
+++ VigenereBreaker.java
@@ -22,17 +22,64 @@ public class VigenereBreaker {
     public void breakVigenere () {
         FileResource fr = new FileResource();
         String message = fr.asString();
-        int[] key = tryKeyLength(message, 5, 'e');
-        VigenereCipher VC = new VigenereCipher(key);
-        String messagedecrypted  = VC.decrypt(message);
-        System.out.println(messagedecrypted);
+        FileResource di = new FileResource();
+        HashSet<String> dictionary = readDictionary(di);
+        String decrypted = breakForLanguage(message, dictionary);
+        System.out.println(decrypted);
+        System.out.println(countWords(decrypted, dictionary));
+    }
+    
+    public HashSet<String> readDictionary(FileResource fr){
+        HashSet<String> dictionary = new HashSet<String>();
+        for (String line : fr.lines()){
+            String word = line.toLowerCase();
+            dictionary.add(word);
+        }
+        return dictionary;
+    }
+    
+    public int countWords(String message, HashSet<String> dictionary){
+        int sum = 0;
+        String[] words = message.split("\\W+");
+        for (int i = 0; i < words.length; i++){
+            String word = words[i].toLowerCase();
+            if (dictionary.contains(word)){
+                sum += 1;
+            }
+        }
+        return sum;
+    }
+    
+    public String breakForLanguage(String encrypted, HashSet<String> dictionary){
+        int maxi = 0;
+        String result = "";
+        int klength = 0;
+        for (int i = 1; i < 101; i++){
+            int[] key = tryKeyLength(encrypted, i, 'e');
+            VigenereCipher VC = new VigenereCipher(key);
+            String message = VC.decrypt(encrypted);
+            if (countWords(message, dictionary) > maxi){
+                maxi = countWords(message, dictionary);
+                result = message;
+                klength = key.length;
+            }
+        }
+        System.out.println(klength);
+        return result;
     }
     
     public void test(){
         //System.out.println(sliceString("abcdefghijklm", 1, 4));
         //FileResource fr = new FileResource();
-        //String message = fr.asString();
-        //System.out.println(Arrays.toString(tryKeyLength(message, 5, 'e')));
+        //String encrypted = fr.asString();
+        //FileResource di = new FileResource();
+        //HashSet<String> dictionary = readDictionary(di);
+        //System.out.println(Arrays.toString(tryKeyLength(message, 4, 'e')));
+        //System.out.println(countWords(message, dictionary));
+        //int[] key = tryKeyLength(encrypted, 38, 'e');
+        //VigenereCipher VC = new VigenereCipher(key);
+        //String message = VC.decrypt(encrypted);
+        //System.out.println(countWords(message,dictionary));
         breakVigenere();
     }
 }
